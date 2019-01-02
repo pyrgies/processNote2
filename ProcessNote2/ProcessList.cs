7e9aa7a50f4ea4fc7801aed1bfce0a5047c3fee2
pyrgies <albert@wysoka.org>
2019-01-02 11:48:29 +0100

    Elo

---
+++ ProcessNote2/ProcessList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessNote2
+{
+    class ProcessList
+    {
+        // creating List with objects of type Process
+
+        private List<Process> processesList = new List<Process>(Process.GetProcesses());
+
+        public List<Process> ProcessesList
+        {
+            get => processesList;
+            set => processesList = value;
+        }
+    }
+}
---
+++ ProcessNote2/ProcessListVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessNote2
+{
+    class ProcessListVM
+    {
+        private static ProcessList processesListObject = new ProcessList();
+
+        public List<string> Names { get; set; } = new List<string>();
+
+        public void LoadNames()
+        {
+            foreach (var process in processesListObject.ProcessesList)
+            {
+                Names.Add(process.ProcessName);
+            }
+        }
+    }
+}
